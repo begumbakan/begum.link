2d753d61b5fd088a972d740fb18e5592bc53732f
Begüm Bakan <begumbakan2005@gmail.com>
2026-06-22 22:54:18 +0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import './App.css'
 
 export default function App() {
   return (
+  <>
+    <nav>
+      <span className="nav-logo">Begüm Bakan</span>
+      <ul>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
     <div className="world">
       <Character />
     </div>
-  )
+  </>
+)
+
 }
